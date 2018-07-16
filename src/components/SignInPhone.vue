ace1347aa7265f6946983b5ba248539f0d18073c
Felix Lennartsson <felix@fel.is>
2018-07-16 17:11:31 +0200

    Componetised and improved phone login flow

--- src/components/SignInPhone.vue
+++ src/components/SignInPhone.vue
@@ -1,36 +1,161 @@
 <template>
-    <div class="sign-in-phone">
-        <h3>Logga in med SMS-kod</h3>
-        <form @submit.prevent="login">
-            <input type="tel" placeholder="Mobilnummer" v-model="user.mobile"><br>
-            <button type="submit">Logga in</button>
-        </form>
+    <div id="sign-in-phone">
+        <v-dialog v-model="mobileDialog" max-width="290" persistent>
+            <v-card>
+                <v-card-title class="headline">
+                    Ange telefonnummer
+                </v-card-title>
+                <v-card-text>
+                    <div class="caption pb-3">
+                        Ange ditt telefonnummer som du har kopplat för att logga in.
+                    </div>
+                    <form @submit.prevent id="mobile-form">
+                        <v-text-field
+                            type="tel"
+                            v-model.trim="mobile"
+                            ref="mobileInput"
+                            clearable
+                            label="Telefonnummer"
+                            prepend-icon="phone" />
+                    </form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                        flat
+                        color="red"
+                        @click="abort">
+                        Avbryt
+                    </v-btn>
+                    <v-btn
+                        form="mobile-form"
+                        id="mobile-submit"
+                        type="submit"
+                        flat
+                        color="green">
+                        Fortsätt
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="confirmationCodeDialog" max-width="290" persistent>
+            <v-card>
+                <v-card-title class="headline">
+                    Ange bekräftelsekod
+                </v-card-title>
+                <v-card-text>
+                    <div class="caption pb-3">
+                        Du bör inom kort få en bekräftelsekod till ditt nummer <strong>{{ mobile }}</strong>. Ange koden för att fortsätta.
+                    </div>
+                    <form @submit.prevent="confirmationCodeSubmit" id="confirmation-code-form">
+                        <v-text-field
+                            prepend-icon="confirmation_number"
+                            type="tel"
+                            :loading="confirmationCode.length > 0 && confirmationCodeStatus.status == null"
+                            v-model.trim="confirmationCode"
+                            ref="confirmationCodeInput"
+                            label="Bekräftelsekod"
+                            @input="(confirmationCode.length === 6) ? confirmationCodeSubmit() : null" />
+                    </form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                        flat
+                        color="red"
+                        @click="abort">
+                        Avbryt
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
 <script>
-    import { auth } from '@/plugins/firebase'
-    
+    import { firebase, auth } from '@/plugins/firebase'
+
     export default {
-        name: 'signInEmailPass',
         data() {
             return {
-                user: {
-                    email: '',
-                    password: '',
-                    accessModules: ['admin', 'profile'] //Should be moved!
+                mobileDialog: false,
+                mobile: '',
+                confirmationCodeDialog: false,
+                confirmationCode: '',
+                confirmationCodeStatus: {
+                    status: null
                 }
             }
         },
-        methods: {
-            async login() {
-                try {
-                    await auth.signInWithEmailAndPassword(this.user.email, this.user.password)
-                    this.$emit('signInSuccess', this.user)
-                } catch(err) {
-                    alert('Ett fel inträffade! Felmeddelande: ' + err.message)
+        props: ['phone'],
+        watch: {
+            phone: function(value) {
+                if (value) {
+                    document.getElementsByClassName('grecaptcha-badge')[0].setAttribute('style', 'display: none !important')
+                    this.$nextTick(this.$refs.mobileInput.focus)
                 }
+                this.mobileDialog = value
             }
+        },
+        methods: {
+            mobileSubmit() {
+                auth.signInWithPhoneNumber(this.mobile, window.recaptchaVerifier)
+                    .then(confirmationResult => {
+                        window.confirmationResult = confirmationResult
+                        grecaptcha.reset(window.recaptchaWidgetId);
+                        this.mobileDialog = false
+                        this.$nextTick(this.$refs.confirmationCodeInput.focus)
+                        this.confirmationCodeDialog = true
+                    }).catch(error => {
+                        alert('Error in sign in with phone number')
+                        console.log(error.code)
+                        console.log(error.message)
+                })
+            },
+            confirmationCodeSubmit() {
+                confirmationResult.confirm(this.confirmationCode)
+                    .then(result => {
+                        this.confirmationCodeStatus.status = 'success'
+                        let user = result.user
+                        this.mobile = ''
+                        this.confirmationCode = ''
+                        this.confirmationCodeDialog = false
+                        //alert('User successful sign in. UID is ' + user.uid)
+                    }).catch(error => {
+                        this.confirmationCodeStatus.status = 'error'
+                        alert('Error in confirmation of otp')
+                        console.log(error.code)
+                        console.log(error.message)
+                    })
+            },
+            abort() {
+                grecaptcha.reset(window.recaptchaWidgetId);
+                this.mobile = ''
+                this.confirmationCode = ''
+                this.mobileDialog = false
+                this.confirmationCodeDialog = false
+                this.$emit('abort')
+            }
+        },
+        mounted() {
+            window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('mobile-submit', {
+                size: 'invisible',
+                callback: response => {
+                    this.mobileSubmit()
+                }
+            })
+            window.recaptchaVerifier.render().then((widgetId) => {
+                window.recaptchaWidgetId = widgetId
+            })
         }
     }
 </script>
+
+<style scoped>
+    .grecaptcha-badge,
+    .grecaptcha-logo 
+    {
+        display: none !important;
+    }
+</style>
--- src/views/Landing/SignIn.vue
+++ src/views/Landing/SignIn.vue
@@ -12,9 +12,9 @@
       <v-flex xs12 md4>
         <v-layout column justify-center align-center>
           <v-flex xs12>
-            <v-btn id="google-sign-in-button" large color="white">
+            <v-btn class="sign-in-button" large color="white">
               <v-avatar>
-                <img src="http://diylogodesigns.com/blog/wp-content/uploads/2016/04/google-logo-icon-PNG-Transparent-Background.png">
+                <img src="@/assets/google-logo.png">
               </v-avatar>
               &nbsp;&nbsp;&nbsp;Logga in med Google
             </v-btn>
@@ -25,49 +25,19 @@
           </v-flex>
           
           <v-flex xs12>
-            <v-form @submit.prevent id="mobile-form">
-              <v-text-field style="display: inline-block"
-                id="mobile-form-input"
-                v-model.trim="mobile"
-                solo
-                clearable
-                prepend-inner-icon="phone"
-                persistent-hint
-                hint="Ange ett kopplat nummer ovan för att logga in" 
-                placeholder="Logga in med SMS"
-                @input="mobileInput" />
-              <v-btn type="submit" id="mobile-form-submit" icon style="display: inline-block; margin-bottom: 22px" v-show="mobileSendButton">
-                <v-icon color="primary">send</v-icon>
-              </v-btn>
-            </v-form>
-            
-            <v-dialog v-model="dialog" max-width="290" persistent>
-              <v-card>
-                <v-card-title class="headline">
-                  Ange bekräftelsekoden från SMS
-                </v-card-title>
-                <v-card-text>
-                  <div class="caption pb-3">Du bör inom kort få en bekräftelsekod skickad till dig på <strong>{{ mobile }}</strong> som du anger nedan</div>
-                  <form @submit.prevent="confirmationCodeSubmit">
-                    <v-text-field 
-                      v-model.trim="confirmationCode"
-                      autofocus
-                      clearable
-                      label="Bekräftelsekod" />
-                  </form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn flat color="red" @click="confirmationCodeAbort">
-                    Avbryt
-                  </v-btn>
-                  <v-btn flat color="green" @click="confirmationCodeSubmit">
-                    Fortsätt
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <v-btn 
+              class="sign-in-button" 
+              large 
+              color="white"
+              @click="showPhone = true">
+              <v-avatar>
+                <v-icon large color="primary">phone</v-icon>
+              </v-avatar>
+              &nbsp;&nbsp;&nbsp;Logga in med SMS
+            </v-btn>
+            <SignInPhone :phone="showPhone" @abort="showPhone = false" />
           </v-flex>
+
         </v-layout>
       </v-flex>
     </v-layout>
@@ -76,179 +46,22 @@
 
 <script>
   import { firebase, auth } from '@/plugins/firebase'
-  
+  import SignInPhone from '@/components/SignInPhone.vue'
+
   export default {
     data() {
       return {
-        mobileSendButton: false,
-        mobile: '',
-        dialog: false,
-        confirmationCode: ''
+        showPhone: false
       }
     },
-    methods: {
-      mobileInput() {
-        (this.mobile != null && this.mobile.length > 0) ? this.mobileSendButton = true : this.mobileSendButton = false
-      },
-      mobileSubmit() {
-        console.log('here1')
-        let phoneNumber = this.mobile
-        let appVerifier = window.recaptchaVerifier
-        auth.signInWithPhoneNumber(phoneNumber, appVerifier)
-          .then(confirmationResult => {
-            console.log('here2')
-            window.confirmationResult = confirmationResult
-            this.dialog = true
-            console.log('here3')
-          }).catch(error => {
-            console.log('Error in sign in with phone number')
-            console.log(error.code)
-            console.log(error.message)
-        })
-      },
-      confirmationCodeAbort() {
-        grecaptcha.reset(window.recaptchaWidgetId);
-        this.confirmationCode = ''
-        this.mobile = ''
-        this.mobileInput()
-        this.dialog = false
-      },
-      confirmationCodeSubmit() {
-        this.dialog = false
-        this.mobile = ''
-        this.mobileInput()
-        let code = this.confirmationCode
-        confirmationResult.confirm(code)
-          .then(result => {
-            let user = result.user
-            alert('User successful sign in. UID is ' + user.uid)
-        })
-        this.confirmationCode = ''
-      }
-    },
-    mounted() {
-      window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('mobile-form-submit', {
-        size: 'invisible',
-        callback: response => {
-          this.mobileSubmit()
-        }
-      })
-
-      window.recaptchaVerifier.render().then((widgetId) => {
-        window.recaptchaWidgetId = widgetId
-        document.getElementById('mobile-form').children[1].setAttribute('style', 'display: none !important;')
-      })
+    components: {
+      SignInPhone
     }
   }
 </script>
 
 <style scoped>
-  .rounded-outline-card {
-    border: 1px solid rgba(0,0,0,0.12);
-    border-radius: 45px;
-  }
-  #google-sign-in-button {
+  .sign-in-button {
     height: 60px;
   }
-  #mobile-form {
-    display: inline-block;
-  }
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar dark color="primary">
-            <v-toolbar-title>Logga in</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form @submit.prevent="handle">
-              <v-text-field v-model.trim="user.email" prepend-icon="person" label="E-post" type="email" />
-              <v-text-field v-model.trim="user.password" prepend-icon="lock" label="Lösenord" type="password" />
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="primary">Logga in</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
-</template>
-
-
--->
-
-<!--<template>
-  <div class="sign-in">
-    <div id="landing-nav">
-      <router-link to="/sign-in">Logga in</router-link> | 
-      <router-link to="/about">Om</router-link> | 
-    </div>
-    <h1>Logga in</h1>
-    <p>Här ska en användare kunna logga in med olika inloggningsmetoder. Förslag är: SMS-inloggning och Google</p>
-    <SignInEmailPass @signInSuccess="signInSuccess" />
-  </div>
-</template>
-
-<script>
-//import SignInEmailPass from '@/components/SignInEmailPass.vue'
-
-export default {
-  /*components: {
-    SignInEmailPass
-  },*/
-  data() {
-    return {
-      user: {
-        email: null,
-        password: null
-      }
-    }
-  },
-  methods: {
-    signInSuccess(user) {
-      const possibleTargetPath = this.$route.params.target
-      if (typeof possibleTargetPath !== 'undefined') {
-        this.$router.replace(possibleTargetPath)
-      } else {
-        this.$router.replace({ name: user.accessModules[0] })
-      }
-    },
-    handle() {
-      alert('Yay, sign in!')
-    }
-  }
-}
-</script>-->
